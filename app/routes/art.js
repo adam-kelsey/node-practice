a1261233eefc8549044b4e99eac777dc1de5ccdf
Adam Kelsey <adam.kelsey113@gmail.com>
2016-10-12 13:30:28 -0400

    artworklist page now showing, images are there but not displaying

--- app/routes/art.js
+++ app/routes/art.js
@@ -3,9 +3,16 @@ var router = express.Router();
 
 router.get('/art', function(req, res) {
   var data = req.app.get('appData');
+  var pageArt = [];
+  var pageArtwork = data.artwork;
+
+  data.artwork.forEach(function(item) {
+    pageArt = pageArt.concat(item.artwork);
+  });
 
   res.render('art', {
     pageTitle: 'Art',
+    artwork: pageArt,
     pageID: 'art'
   });
 
