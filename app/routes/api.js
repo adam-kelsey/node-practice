22a004eff34336a0b849cc5c407e42e4993cac56
Adam Kelsey <adam.kelsey113@gmail.com>
2016-10-20 16:14:51 -0400

    fixed error with api route, route itself set up

--- app/routes/api.js
+++ app/routes/api.js
@@ -5,5 +5,20 @@ var fs = require('fs');
 var postingData = require('../data/postingdata.json');
 
 router.get('/api', function(req, res) {
-
+  res.json(postingData);
 });
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false}));
+
+router.post('/api', function(req, res) {
+  postingData.unshift(req.body);
+  fs.writeFile('app/data/postingdata.json', JSON.stringify(postingData), 'utf8', function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  res.json(postingData);
+});
+
+module.exports = router;
