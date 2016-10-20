16acaf6cace62b72a67f410079dc1c761101e706
Adam Kelsey <adam.kelsey113@gmail.com>
2016-10-20 15:15:30 -0400

    in the process of building API route to handle posts

--- app/app.js
+++ app/app.js
@@ -13,6 +13,7 @@ app.use(express.static('app/public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/art'));
 app.use(require('./routes/photography'));
+app.use(require('./routes/api'));
 
 var server = app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'));
---
+++ app/routes/api.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var postingData = require('../data/postingdata.json');
+
+router.get('/api', function(req, res) {
+
+});
