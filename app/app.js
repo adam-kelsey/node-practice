33377487ab3bb5f276c21070f458096a52427ff1
Adam Kelsey <adam.kelsey113@gmail.com>
2016-10-25 16:42:16 -0400

    temporary routes to make sure login is installed/working correctly

--- app/app.js
+++ app/app.js
@@ -1,6 +1,24 @@
 var express = require('express');
 var app = express();
 var dataFile = require('./data/data.json');
+var passport = require('passport')
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
 
 app.set('port', process.env.PORT || 3000);
 app.set('appData', dataFile);
@@ -15,6 +33,17 @@ app.use(require('./routes/art'));
 app.use(require('./routes/photography'));
 app.use(require('./routes/api'));
 app.use(require('./routes/submitions'));
+
+app.get('/login',
+  function(req, res){
+    res.render('login');
+  });
+  
+app.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
 
 var server = app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'));
