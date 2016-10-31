58512b513079ae4f0dc4bde1d92086d3d9fd2a9e
Adam Kelsey <adam.kelsey113@gmail.com>
2016-10-31 16:57:22 -0400

    submitions.js being built, still not writing file correctly

--- app/public/js/submitions.js
+++ app/public/js/submitions.js
@@ -6,6 +6,21 @@ $(function() {
     $.post('api', {
       name: $('#submition-form-name').val(),
       title: $('#submition-form-title').val(),
-      message: $('#submition-form-message').val()
     }, updateSubmitions);
   });
+
+  function updateSubmitions(data) {
+    var output = '';
+    $.each(data,function(key, item) {
+
+      output += '          <div class="submition-info media-body">';
+      output += '            <div class="submition-head">';
+      output += '              <div class="submition-title">' + item.name + ' <small class="submition-name label label-info">' + item.title + '</small></div>';
+      output += '            </div>';
+      output += '          </div>';
+      output += '        </div>';
+      output += '      </div>';
+    });
+    $('.submition-messages').html(output);
+  }
+});
