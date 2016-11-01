a152b7828db4a3f39a6238bc95bca4ad9d62e8d9
Adam Kelsey <adam.kelsey113@gmail.com>
2016-11-01 15:57:07 -0400

    update submitions funciton updated for delete, can't test functionality yet

--- app/public/js/submitions.js
+++ app/public/js/submitions.js
@@ -23,11 +23,19 @@ $(function() {
     var output = '';
     $.each(data,function(key, item) {
 
+      output += '      <div class="submition-item item-list media-list">';
+      output += '        <div class="submition-item media">';
+      output += '        <div class="media-left"><button class="submition-delete btn btn-xs btn-danger"><span id="' + key + '" class="btn btn-danger"></span></button></div>';
+
+
+
       output += '          <div class="submition-info media-body">';
       output += '            <div class="submition-head">';
       output += '              <div class="submition-title">' + item.name + ' <small class="submition-name label label-info">' + item.title + '</small></div>';
       output += '            </div>';
       output += '          </div>';
+      output += '        </div>';
+      output += '      </div>';
     });
     $('.submition-messages').html(output);
   }
