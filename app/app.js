24b0f6e092215e807183ea456148898bb29e4f50
Adam Kelsey <adam.kelsey113@gmail.com>
2016-11-04 16:59:11 -0400

    sql set up and working

--- app/app.js
+++ app/app.js
@@ -7,17 +7,19 @@ var passport = require('passport')
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'me',
-  password : 'secret',
+  port     : '3000',
+  user     : 'adam',
+  password : '',
   database : 'my_db'
 });
 
-connection.connect();
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
+  console.log('connected as id ' + connection.threadId);
 });
 
 connection.end();
