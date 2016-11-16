6546b0f77febfa9ed0f7f37b31bd2464adc169df
Adam Kelsey <adam.kelsey113@gmail.com>
2016-11-16 15:16:14 -0500

    added the rest of the function for bcrypt

--- app/public/js/functions.js
+++ app/public/js/functions.js
@@ -39,3 +39,34 @@ exports.localReg = function (username, password) {
 
   return deferred.promise;
 };
+
+//check if user exists
+    //if user exists check if passwords match (use bcrypt.compareSync(password, hash); // true where 'hash' is password in DB)
+      //if password matches take into website
+  //if user doesn't exist or password doesn't match tell them it failed
+exports.localAuth = function (username, password) {
+  var deferred = Q.defer();
+
+  db.get('local-users', username)
+  .then(function (result){
+    console.log("FOUND USER");
+    var hash = result.body.password;
+    console.log(hash);
+    console.log(bcrypt.compareSync(password, hash));
+    if (bcrypt.compareSync(password, hash)) {
+      deferred.resolve(result.body);
+    } else {
+      console.log("PASSWORDS NOT MATCH");
+      deferred.resolve(false);
+    }
+  }).fail(function (err){
+    if (err.body.message == 'The requested items could not be found.'){
+          console.log("COULD NOT FIND USER IN DB FOR SIGNIN");
+          deferred.resolve(false);
+    } else {
+      deferred.reject(new Error(err));
+    }
+  });
+
+  return deferred.promise;
+}
