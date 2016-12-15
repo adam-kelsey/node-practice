eeb1b876a0756d86c90a17e6b1e493291256e0cf
Adam Kelsey <adam.kelsey113@gmail.com>
2016-12-15 16:40:05 -0500

    added commented out user auth to a temporary spot

--- app/routes/submitions.js
+++ app/routes/submitions.js
@@ -9,4 +9,11 @@ router.get('/submitions', function(req, res) {
   });
 });
 
+// Simple route middleware to ensure user is authenticated.
+//function ensureAuthenticated(req, res, next) {
+//  if (req.isAuthenticated()) { return next(); }
+//  req.session.error = 'Please sign in!';
+//  res.redirect('/signin');
+//}  Temporary spot for middleware authentication code. Will move to appropriate spot when login is working properly
+
 module.exports = router;
